713469713e3241ecc371d516b3e208a7cb7fdd3d
Olha Maievska <olhamaievska91@gmail.com>
2024-06-15 14:17:52 +0200

    fixed bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import RightSide from './components/RightSilde/Index'
 
 function App() {
   return (
-    <div className="relative bg-blue sm:bg-white  sm:before:bg-blue sm:before:absolute sm:before:left-0 sm:before:top sm:before:w-[400px] lg:before:w-[900px] before:h-full before:-z-10">
+    <div className="relative bg-blue sm:bg-transparent before:hidden sm:before:block  before:bg-blue before:absolute before:left-0 before:top before:w-full sm:before:w-[400px] lg:before:w-[900px] before:h-full before:-z-10">
       <div className="container font-main">
         <div className="block sm:flex justify-between">
           <LeftSide />
